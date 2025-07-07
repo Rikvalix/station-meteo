4acaf745181566b71a1db8956517045a3437a2d4
Rikvalix <rikvalix@github.com>
2025-07-07 11:09:30 +0200

    feat: env exemple, docker compose mongodb

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -1,11 +1,13 @@
-package server
+package main
 
 import (
+	"errors"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"os"
 	"station_meteo_api/internal/config"
+	"station_meteo_api/internal/router"
 )
 
 func main() {
@@ -16,10 +18,12 @@ func main() {
 	logger := config.NewLogger()
 
 	e := echo.New()
-	
+
+	router.InitRoutes(e)
+
 	port := os.Getenv("SERVER_PORT")
 	logger.Infof("Serveur lancé sur http://localhost%s", port)
-	if err := e.Start(port); err != nil && err != http.ErrServerClosed {
+	if err := e.Start(port); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		logger.Fatalf("Erreur au démarrage du serveur : %v", err)
 	}
 }
