7497b26abe037201b46e13c28cc78e1073d6d22d
Rikvalix <rikvalix@github.com>
2025-07-19 12:20:52 +0200

    feat: utilisateur par défaut

--- mongodb/initdb.d/initdb.js
+++ mongodb/initdb.d/initdb.js
@@ -2,3 +2,11 @@ db = db.getSiblingDB("station_meteo")
 
 db.createCollection("mesures")
 db.createCollection("stations")
+db.createCollection("utilisateurs")
+
+// Utilisateur par défaut
+// mp = beautifulPassword
+db.utilisateurs.insertOne({
+    pseudo:"root",
+    password:"$2a$12$JAxmd2Ha1c7iMAt2PDdtlORf/J9/kLvGgI/AN/53chAPlKmUaASja"
+})
